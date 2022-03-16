9711f368158f8ab471ef5223489286a19f44582d
kevinderitis <32942115+kevinderitis@users.noreply.github.com>
2022-03-15 21:10:33 -0300

    Update ej3.js

--- ej3.js
+++ ej3.js
@@ -1,8 +1,12 @@
 import moment from 'moment';
 
 
-
-let fecha1 = moment('1992-12-10');
-let fecha2 = moment('2021-11-29');
+let fecha1 = moment('2022-03-15'); 
+let fecha2 = moment('1992-12-10');
 
 console.log(fecha2.diff(fecha1, 'days'), ' dias de diferencia');
+
+console.log(`Hoy es ${fecha1.format('L')}`)
+console.log(`Naci el ${fecha2.format('L')}`)
+console.log(`Desde mi nacimiento han pasado ${fecha1.diff(fecha2, 'days')} dias`)
+console.log(`Desde mi nacimiento han pasado ${fecha1.diff(fecha2, 'years')} años`)
